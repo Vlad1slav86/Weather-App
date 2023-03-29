6e20ec93e9c7e591ebe3f213fcd7165ff6a73a70
VlaD <vladkb@yahoo.com>
2023-03-29 11:44:16 -0700

    updated html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
 console.log("js is working");
+
+//https://api.openweathermap.org/data/2.5/weather?appid=${bfc55248141e933e27bdb8ff269870d8}&q=${London}&units=imperial
+
+//https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apikey}
+
