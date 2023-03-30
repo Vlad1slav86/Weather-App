79f763ba517f3cd75c9da1ff3d00be7575be9793
VlaD <vladkb@yahoo.com>
2023-03-30 09:00:48 -0700

    added consts to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,16 @@ console.log("js is working");
 
 //https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apikey}
 
+// select the form and input element
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+
+// select the elements that will display weather data
+const cityName = document.querySelector('#city-name');
+const date = document.querySelector('#date');
+const temperature = document.querySelector('#temperature');
+const humidity = document.querySelector('#humidity');
+const windSpeed = document.querySelector('#wind-speed');
+const weatherIcon = document.querySelector('#weather-icon');
+
+
