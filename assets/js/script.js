22571137aa55faad834b92fa90661955a3fefcb4
Vlad1slav86 <121739579+Vlad1slav86@users.noreply.github.com>
2023-04-03 21:19:55 -0700

    removed old comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-console.log("js is working");
-
-//https://api.openweathermap.org/data/2.5/weather?appid=${bfc55248141e933e27bdb8ff269870d8}&q=${London}&units=imperial
-
-//https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apikey}
-
 // select the form and input element
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
